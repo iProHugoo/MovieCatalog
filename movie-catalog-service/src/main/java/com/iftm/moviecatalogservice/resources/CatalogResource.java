ef83d96decee41b5e662243e96c665e17d3e4845
Hugo Coelho <iProHugoo@outlook.com>
2021-11-10 11:15:31 -0300

    Criando RestController e Model para Catalog Service

---
+++ movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
@@ -0,0 +1,23 @@
+package com.iftm.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iftm.moviecatalogservice.models.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class CatalogResource {
+
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+		return Collections.singletonList(new CatalogItem("Velozes e Furiosos", "Filme de Corrida", 10));
+
+	}
+
+}
